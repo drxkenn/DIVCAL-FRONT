b00a12762a1bb5625d9b7963756e599b422c3e7f
Edgard Quijano <dabertquijano@gmail.com>
2026-01-08 10:46:21 -0500

    POST en formulario

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,12 +1,14 @@
 // @ts-check
 import { defineConfig } from 'astro/config';
-
 import tailwindcss from '@tailwindcss/vite';
-
 import react from '@astrojs/react';
+import node from '@astrojs/node';
 
 // https://astro.build/config
 export default defineConfig({
+  output: "server", // 👈 CLAVE para que funcione /api
+  adapter: node({ mode: "standalone" }), // 👈 servidor Node
+
   vite: {
     plugins: [tailwindcss()]
   },
